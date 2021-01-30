0600b27af95497d7f291172fb1e303cdb7660c09
paucamos <paucamos98@gmail.com>
2021-01-30 12:09:21 +0100

    Initial Commit

---
+++ cpp-random-selector-v1/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  cpp-random-selector-v1
+//
+//  Created by pablodev on 30/1/21.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
